81aec73a0afe784df129980cd75a2c4fd026820a
lorincet <lorincet@gmail.com>
2022-01-04 18:32:05 +0100

    Ajout de la couche API

---
+++ src/main/java/tech/satic/onlineshop/customer/web/CustomerRestAPI.java
@@ -0,0 +1,31 @@
+// Package
+package tech.satic.onlineshop.customer.web;
+
+// Librairie
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.satic.onlineshop.customer.services.CustomerService;
+
+/**
+ * Couche web d'accès aux API.
+ * @version     1.0.0
+ * @since       04/01/2022
+ * @author      TAWAMBA KOUAKAM Lorince S.
+ */
+@RestController
+@RequestMapping(path = "/customer/api")
+public class CustomerRestAPI {
+
+    private CustomerService customerService;
+
+    public CustomerRestAPI(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping(path = "/info")
+    public String info() {
+        return "SATIC ONLINE SHOP CUSTOMER SERVICE";
+    }
+}
